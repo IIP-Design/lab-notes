07327afb789e1adf5e6ef8807241c31adc3a4da1
mrewers <mrewers@users.noreply.github.com>
2020-02-07 11:17:42 -0500

    Add babel and webpack to transpile JS

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'assets/js')
+  },
+  module: {
+    rules: [{ exclude: /node_modules/, test: /\.js$/, use: 'babel-loader' }]
+  }
+};
