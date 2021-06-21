ac4dd3207b125e5b8adc5067f79973ee6cea2ca7
mrewers <hello@marek.dev>
2021-06-20 23:38:23 -0400

    feat: track the user's progress through a page using the table of contents

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import { initHighlighting } from './highlight';
-import { buildTOC } from './toc';
+import { initializeTOC } from './toc';
 import { navToggleListener } from './menu-toggle';
 import { ready } from './document-ready';
 
@@ -9,6 +9,6 @@ initHighlighting();
  * Set up the page event listeners once the page is loaded.
  */
 ready( () => {
-  buildTOC();
+  initializeTOC();
   navToggleListener();
 } );
--- js/toc.js
+++ js/toc.js
@@ -1,3 +1,7 @@
+/**
+ * Retrieves a list of all heading elements on the page.
+ * @returns {HTMLElement[]} The headings on the page listed in order from the top of the page.
+ */
 const getHeadings = () => {
   // List of headings to search for.
   const hs = [
@@ -19,14 +23,90 @@ const getHeadings = () => {
   return headings;
 };
 
-export const buildTOC = () => {
-  const wrapper = document.getElementById( 'toc' );
+/**
+ * Adds the class "active" to a given element.
+ * @param {HTMLElement} el The targeted DOM element.
+ */
+const makeActive = el => {
+  el.classList.add( 'active' );
+};
 
+/**
+ * Removes the class "active" to a given element.
+ * @param {HTMLElement} el The targeted DOM element.
+ */
+const makeInactive = el => {
+  el.classList.remove( 'active' );
+};
+
+/**
+ * Checks whether the current scroll position is at or past a provided y-offset position.
+ * @param {number} current The current vertical position on the page.
+ * @param {number} target The vertical position to test against.
+ * @returns {boolean} Whether the page is scrolled past the provided value.
+ */
+const passedOffset = ( current, target ) => current >= target;
+
+/**
+ * Toggles the active class on and off of TOC links as the user scrolls down the page.
+ * @param {number} offset The current vertical position on the page.
+ * @param {HTMLElement[]} links List of the links in the table of contents.
+ */
+const trackInToc = ( offset, links ) => {
+  links.forEach( ( link, idx ) => {
+    // Retrieve the y-offset of the next link and check whether it has been passed.
+    const next = links[idx + 1];
+    const passedNext = next?.dataset?.offset ? passedOffset( offset, next.dataset.offset ) : false;
+
+    if ( idx === 0 && !passedOffset( offset, link.dataset.offset ) ) {
+      // First link in TOC & not passed.
+      makeActive( link );
+    } else if ( passedOffset( offset, link.dataset.offset ) && !passedNext ) {
+      // Has been passed and next link has not been passed.
+      makeActive( link );
+    } else {
+      makeInactive( link );
+    }
+  } );
+};
+
+/**
+ * Checks the vertical position on the page, toggling the active links as appropriate.
+ */
+const addScrollListener = () => {
+  let lastKnownScrollPosition = 0;
+  let ticking = false;
+
+  document.addEventListener( 'scroll', () => {
+    lastKnownScrollPosition = window.scrollY;
+
+    if ( !ticking ) {
+      window.requestAnimationFrame( () => {
+        const links = document.querySelectorAll( '.toc-link' );
+
+        trackInToc( lastKnownScrollPosition, links );
+
+        ticking = false;
+      } );
+
+      ticking = true;
+    }
+  } );
+};
+
+/**
+ * Convert a list of headings into the linked-list of the table of contents.
+ * @param {HTMLElement[]} headings The list of headings on the page.
+ * @returns {HTMLElement} A ul element with each heading converted into an anchor link.
+ */
+const buildTOC = headings => {
+  // Create ul to wrap list of links.
   const list = document.createElement( 'ul' );
 
   list.classList.add( 'toc-list' );
 
-  getHeadings().forEach( heading => {
+  // Iterate through headings creating a link for each one.
+  headings.forEach( ( heading, idx ) => {
     const link = document.createElement( 'a' );
 
     link.innerHTML = heading.innerHTML;
@@ -34,11 +114,49 @@ export const buildTOC = () => {
     link.classList.add( 'toc-link' );
     link.dataset.offset = heading.offsetTop;
 
+    // Initialize the first link as active.
+    if ( idx === 0 ) {
+      makeActive( link );
+    }
+
     const item = document.createElement( 'li' );
 
     item.appendChild( link );
     list.appendChild( item );
   } );
 
-  wrapper.appendChild( list );
+  return list;
+};
+
+/**
+ * Construct the table of contents and add the scroll event listener to control it's behavior.
+ */
+export const initializeTOC = () => {
+  // Ensure that the TOC element is present on the page.
+  const wrapper = document.getElementById( 'toc' );
+
+  if ( wrapper ) {
+    const headings = getHeadings();
+
+    // Only construct the table of contents if there is more than one heading on the page.
+    if ( headings.length > 1 ) {
+      // If populated, make TOC visible to screen readers.
+      wrapper.setAttribute( 'aria-hidden', 'false' );
+
+      // Add title to the floating table of contents.
+      const title = document.createElement( 'strong' );
+
+      title.innerHTML = 'Table of Contents';
+
+      wrapper.appendChild( title );
+
+      // Populate the contents of the TOC.
+      const toc = buildTOC( headings );
+
+      wrapper.appendChild( toc );
+
+      // Add scrolling behavior.
+      addScrollListener();
+    }
+  }
 };
